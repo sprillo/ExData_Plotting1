320058660a711abec1f6959f10ddeb634fd77e25
sprillo <sebastianprillo@gmail.com>
2015-02-03 19:34:10 -0300

    Commented plot1.R and plot2.R

--- plot1.R
+++ plot1.R
@@ -1,17 +1,29 @@
-library(data.table);
+# set wd to directory containing household_power_consumption.txt
 setwd("~/data");
+
+# read household_power_consumption.txt to variable data
 data <- read.table("household_power_consumption.txt",
                    nrows=2075259,
                    header=T,
                    quote="",
                    sep=";",
                    na.strings = "?");
+
+# subset data to days 1/2/2007 and 2/2/2007
 data <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007",];
+
+# set wd to plots directory where we will save the plot
 setwd("~/plots");
+
+# start png device
 png("plot1.png", width = 480, height = 480);
+
+# plot
 hist(data$Global_active_power,
     col = "red",
     main = "Global Active Power",
     xlab = "Global Active Power (kilowatts)",
     ylab = "Frequency");
+
+# done!
 dev.off();
--- plot2.R
+++ plot2.R
@@ -1,19 +1,30 @@
-library(data.table);
+# set wd to directory containing household_power_consumption.txt
 setwd("~/data");
+
+# read household_power_consumption.txt to variable data
 data <- read.table("household_power_consumption.txt",
                    nrows=2075259,
                    header=T,
                    quote="",
                    sep=";",
                    na.strings = "?");
+
+# subset data to days 1/2/2007 and 2/2/2007
 data <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007",];
+
+# replace data$Time values by POSIXlt values resulting from combining data$Date
+# and data$Time
 dateAndTime <- paste(data$Date,data$Time)
 dateAndTime <- strptime(dateAndTime,"%d/%m/%Y %T") 
 data$Time <-dateAndTime
 
+# set wd to plots directory where we will save the plot
 setwd("~/plots");
+
+# start png device
 png("plot2.png", width = 480, height = 480);
 
+# plot
 with(data,
         plot(Time,
              Global_active_power,
@@ -23,4 +34,5 @@ with(data,
              )
      )
 
+# done!
 dev.off();
